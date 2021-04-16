5c48222dae94d3fd41d000efbc5c52038abb2d3e
Matteo Carnelos <matteo.carnelos98@gmail.com>
2021-04-16 23:45:14 +0200

    Add base structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sailtrack-core',
+    version='0.0.1',
+    packages=['sailtrackd'],
+    url='https://github.com/metis-vela-unipd/sailtrack-core',
+    license='GPL-3.0',
+    author='Métis Vela Unipd',
+    author_email='',
+    description='Core package of the SailTrack system.'
+)
