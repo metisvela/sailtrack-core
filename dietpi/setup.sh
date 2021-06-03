65ea8c828c841844b03e2d2af4cf83da3b47f9d1
Matteo Carnelos <matteo.carnelos98@gmail.com>
2021-06-03 17:47:19 +0200

    Update setup script

--- dietpi/setup.sh
+++ dietpi/setup.sh
@@ -1,11 +1,8 @@
 apt install telegraf
-#wget -P /etc/telegraf https://raw.githubusercontent.com/metis-vela-unipd/sailtrack-core/develop/telegraf/telegraf.conf
-#echo '+ telegraf' >> /boot/dietpi/.dietpi-services_include_exclude
-#dietpi-services restart telegraf
-#
-#pip3 install git+https://github.com/metis-vela-unipd/sailtrack-core.git
-#{
-#  echo '+ sailtrackd-imu'
-#  echo '+ sailtrackd-filter'
-#  echo '+ sailtrackd-status'
-#} >> /boot/dietpi/.dietpi-services_include_exclude
+wget -P /etc/telegraf https://raw.githubusercontent.com/metis-vela-unipd/sailtrack-core/main/telegraf/telegraf.conf
+echo '+ telegraf' >> /boot/dietpi/.dietpi-services_include_exclude
+dietpi-services restart telegraf
+
+pip3 install git+https://github.com/metis-vela-unipd/sailtrack-core.git
+echo '+ sailtrackd' >> /boot/dietpi/.dietpi-services_include_exclude
+dietpi-services start sailtrackd
