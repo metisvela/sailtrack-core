2acbeea7f8e25eb49a28477c1f8e69f3c2b03cb3
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2021-06-10 15:15:22 +0200

    Adjust RTC configuration

--- dietpi/Automation_Custom_Script.sh
+++ dietpi/Automation_Custom_Script.sh
@@ -1,8 +1,9 @@
-cd /boot/sailtrack || exit
+# Install and configure RTC Module (DS3231)
+echo dtoverlay=i2c-rtc,ds3231 >> /boot/config.txt
 
 # Install and configure telegraf
 apt install telegraf
-ln -s telegraf/* /etc/telegraf/telegraf.d
+ln -s /boot/sailtrack/telegraf/* /etc/telegraf/telegraf.d
 echo "+ telegraf" >> /boot/dietpi/.dietpi-services_include_exclude
 
 # Install and configure x708
@@ -10,8 +11,8 @@ echo "/boot/sailtrack/x708/pwr.sh &" > /var/lib/dietpi/postboot.d/x708pwr.sh
 echo "alias x708off='sudo /boot/sailtrack/x708/softsd.sh'" >> /etc/bash.bashrc
 
 # Install and configure sailtrackd
-pip3 install -r requirements.txt
-systemctl link systemd/*
+pip3 install -r /boot/sailtrack/requirements.txt
+systemctl link /boot/sailtrack/systemd/*
 echo "+ sailtrackd" >> /boot/dietpi/.dietpi-services_include_exclude
 
 reboot
