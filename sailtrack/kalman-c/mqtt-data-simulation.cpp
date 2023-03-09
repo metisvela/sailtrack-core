890cc8ead2081cdf53ea3a535fd517d8da394a39
Luca-Gale <lucagale13@gmail.com>
2023-03-09 09:27:38 +0100

    fix missing data in .csv

--- sailtrack/kalman-c/mqtt-data-simulation.cpp
+++ sailtrack/kalman-c/mqtt-data-simulation.cpp
@@ -4,23 +4,32 @@
 #include <vector>
 
 #define CSV_NAME    "sensor-data-2022-09-07.csv"
+#define NUM_DATA    22   //columns in the .csv file
 
 using namespace std;
 
 int main() {
     vector<vector<string>> data; 
+
     ifstream file(CSV_NAME); 
     string line;
+
     while (getline(file, line)) {
         vector<string> row; 
         stringstream ss(line);
         string value;
+        int count = 0;
         while (getline(ss, value, ',')) {
             if(value == ""){
                 break;
             }
             row.push_back(value); 
+            count++;
         }
+        while(count < NUM_DATA){        
+            row.push_back("NULL");         
+            count++;                    
+        } 
         data.push_back(row); 
     }
     file.close(); 
@@ -32,9 +41,3 @@ int main() {
     }
     return 0;
 }
-
-
-
-
-
-
